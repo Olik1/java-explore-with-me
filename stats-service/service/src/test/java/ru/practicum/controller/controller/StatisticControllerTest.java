88c6419da1ae76acfc07745d4abeb9496b448213
Olik13 <dorofeeva86@yandex.ru>
2023-08-09 01:24:50 +0300

    Update StatisticControllerTest.java

--- stats-service/service/src/test/java/ru/practicum/controller/controller/StatisticControllerTest.java
+++ stats-service/service/src/test/java/ru/practicum/controller/controller/StatisticControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.practicum.HitRequestDto;
 import ru.practicum.StatsResponseDto;
 import ru.practicum.controller.StatisticController;
